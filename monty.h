aa97bc161fefb69370451709433806a87586b9ed
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-16 12:16:53 -0500

    [update] header with structure

--- monty.h
+++ monty.h
@@ -1,6 +1,10 @@
 #ifndef _MONTY_H_
 #define _MONTY_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -12,9 +16,9 @@
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
 /**
@@ -27,10 +31,25 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/**
+ * laura
+ */
 
+typedef struct vars_st
+{
+    FILE *file;
+    char *buff;
+    size_t sizz;
+    instruction_t *instuctions;
+    stack_t *h;
+    unsigned int line_number;
 
-int getinfo(char *buf, char *opcode, int *val); /* getinfo from data */
-#endif /* _MONTY_H_ */
+} vars;
+void nop(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number); /* print the data of nodes */
+int getinfo(char *buf, char *opcode, int *val);       /* getinfo from data */
+#endif                                                /* _MONTY_H_ */
