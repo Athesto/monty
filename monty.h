77ab761fa10f6259a669b5515ee0cbd74321f3e1
Athesto <gamez.live@gmail.com>
2020-09-15 22:49:44 -0500

    add getinfo proto

--- monty.h
+++ monty.h
@@ -31,4 +31,6 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+
+int getinfo(char *buf, char *opcode, int *val); /* getinfo from data */
 #endif /* _MONTY_H_ */
