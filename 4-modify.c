b4c1064615e8b879c26927d0fb3ed9b97e512d62
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-17 08:09:34 -0500

    [fix] push function

--- 4-modify.c
+++ 4-modify.c
@@ -6,46 +6,40 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-    char *num = NULL;
-    unsigned int i = 0;
-    int neg = 1;
-    stack_t *tmp = NULL, *tm;
+	char *num = NULL;
+	unsigned int i = 0;
+	int neg = 1;
+	stack_t *tmp = NULL;
 
-    num = strtok(NULL, " \t\n");
-    if (!num)
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number), freeAll();
-        exit(EXIT_FAILURE);
-    } /* is a negative number?*/
-    if (num[0] == '-')
-        neg = -1, num++;
-    while (num[i] != '\000' && num[i] != '\n')
-    {
-        if (_isdigit(num[i]) == 0)
-        {
-            fprintf(stderr, "L%u: usage: push integer\n", line_number),
-                freeAll(), exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-    tmp = malloc(sizeof(stack_t));
-    if (!tmp)
-    {
-        fprintf(stderr, "Error: malloc failed\n"), freeAll();
-        exit(EXIT_FAILURE);
-    }
-    tmp->n = neg * atoi(num);
-    tmp->next = NULL;
-    if (!*stack)
-        tmp->prev = NULL, *stack = tmp;
-    else
-    {
-        tm = *stack;
-        while (tm->next)
-            tm = tm->next;
-        tmp->prev = tm;
-        tm->next = tmp;
-    }
+	num = strtok(NULL, " \t\n");
+	if (!num)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number), freeAll();
+		exit(EXIT_FAILURE);
+	} /* is a negative number?*/
+	if (num[0] == '-')
+		neg = -1, num++;
+	while (num[i] != '\000' && num[i] != '\n')
+	{
+		if (_isdigit(num[i]) == 0)
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number),
+				freeAll(), exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	tmp = malloc(sizeof(stack_t));
+	if (!tmp)
+	{
+		fprintf(stderr, "Error: malloc failed\n"), freeAll();
+		exit(EXIT_FAILURE);
+	}
+	tmp->n = neg * atoi(num);
+	tmp->next = NULL;
+	if (!*stack)
+		tmp->prev = NULL, *stack = tmp;
+	else
+		tmp->next = *stack, (*stack)->prev = tmp, *stack = tmp;
 }
 /**
  * pop - ?
@@ -54,8 +48,22 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	stack_t *tmp = *stack;
+
+	(void)line_number;
+	if (!*stack)
+	{
+		fprintf(stderr, "L%u: can´t pop an empty stack\n", line_number), freeAll();
+		exit(EXIT_FAILURE);
+	}
+	while (tmp->next)
+		tmp = tmp->next;
+	if (tmp->prev)
+		tmp->prev->next = NULL;
+	else
+		*stack = NULL;
+
+	free(tmp);
 }
 /**
  * swap - ?
@@ -64,8 +72,8 @@ void pop(stack_t **stack, unsigned int line_number)
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
 /**
  * rotl - ?
@@ -74,8 +82,8 @@ void swap(stack_t **stack, unsigned int line_number)
  */
 void rotl(stack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
 /**
  * rotr - ?
@@ -84,6 +92,6 @@ void rotl(stack_t **stack, unsigned int line_number)
  */
 void rotr(stack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
