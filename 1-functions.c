bf3aa58aa6c8fa8c506fe4db1d0fbebcb2f5c19c
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-17 17:52:41 -0500

    [update] some files

--- 1-functions.c
+++ 1-functions.c
@@ -8,6 +8,10 @@ int start_vars(vars *vars)
 {
 	vars->file = NULL;
 	vars->buff = NULL;
+	vars->sizz = 0;
+	vars->instuctions = NULL;
+	vars->h = NULL;
+	vars->line_number = 0;
 
 	return (EXIT_SUCCESS);
 }
