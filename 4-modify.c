31a3a970a561b1bace9405025ba522d50cee826d
lperezcas16 <lperezcas@unbosque.edu.co>
2020-09-16 20:08:41 -0500

    [update] 4-modify push function

--- 4-modify.c
+++ 4-modify.c
@@ -6,8 +6,48 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+	stack_t *tmp = NULL, *tm;
+	unsigned int i = 0;
+	int neg = 1;
+	char *num = NULL;
+
+	num = strtok(NULL, " \r\t\n");
+	if (num == NULL)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		freeAll();
+		exit(EXIT_FAILURE);
+	} /* is a negative number?*/
+	if (num[0] == '-')
+		neg = -1, num++;
+	while (num[i] != '\000' && num[i] != '\n')
+	{
+		if (_isdigit(num[i]) == 0)
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number),
+				freeAll(), exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	tmp = malloc(sizeof(stack_t));
+	if (!tmp)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		freeAll();
+		exit(EXIT_FAILURE);
+	}
+	tmp->n = neg * atoi(num);
+	tmp->next = NULL;
+	if (!*stack)
+		tmp->prev = NULL, *stack = tmp;
+	else
+	{
+		tm = *stack;
+		while (tm->next)
+			tm = tm->next;
+		tmp->prev = tm;
+		tm->next = tmp;
+	}
 }
 /**
  * pop - ?
